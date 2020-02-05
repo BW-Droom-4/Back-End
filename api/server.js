987ade6da1b1e4756a74e07c3ff97b6e139e6d5e
Krishna Dahal <krishna.dahal2017@gmail.com>
2020-02-05 18:41:55 -0500

    match route endpoints created

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ const userAuthRoute = require('../auth/userAuthRouter');
 const companyAuthRoute = require('../auth/companyAuthRouter');
 const userRoute = require('../routes/user-router.js');
 const companyRoute = require('../routes/company-router.js');
+const matchRoute = require('../routes/match-router.js');
 
 const server = express();
 
@@ -23,6 +24,7 @@ server.use('/api/auth/users', userAuthRoute);
 server.use('/api/auth/companies', companyAuthRoute);
 server.use('/api/users' ,authenticate, userRoute);
 server.use('/api/companies', authenticate, companyRoute);
+server.use('/api/matches', authenticate, matchRoute);
 
 server.use('/', (req, res) => {
     res.send(`<h2>Welcome to Droom-4 API. Register to get access!</h2>`);
