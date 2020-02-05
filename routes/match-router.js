92c327cff42c81bcf208a1cb1d132f9894bb31bf
Krishna Dahal <krishna.dahal2017@gmail.com>
2020-02-05 18:40:46 -0500

    match-router created

---
+++ routes/match-router.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const Matches = require('../helpers/match-model');
+const authenticate = require('../auth/authenticate-middleware');
+
+
+router.get('/', (req, res) => {
+    Matches.getUserLikes()
+    .then( matches => {
+        res.status(201).json(matches)
+    })
+    .catch( err => {
+        res.status(500).json({error: 'Failed to get company likes. Try again later'})
+    })    
+})
+
+module.exports = router;
